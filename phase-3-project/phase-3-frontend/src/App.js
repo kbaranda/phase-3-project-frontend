48a9f4549a932074a93713fc6864d71794df97f2
Kat <bolivarkat@yahoo.com>
2022-02-17 14:26:41 -0600

    Added HomeCards, and Edit page

--- phase-3-project/phase-3-frontend/src/App.js
+++ phase-3-project/phase-3-frontend/src/App.js
@@ -1,10 +1,35 @@
-import Home from "./components/Home";
+import Houses from "./components/Houses";
+import React, {useState, useEffect} from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Edit from "./pages/Edit";
 
 function App() {
+  const [houses, setHouses] = useState([]);
+
+  useEffect(() => {
+   fetch("http://localhost:9292/houses")
+   .then(resp => resp.json())
+   .then(data => setHouses(data))
+ }, [])
+
+ const handleDeleteHouse = (id) => {
+  const updatedHouses = houses.filter((house) => house.id !== id);
+  setHouses(updatedHouses)
+}
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/edit">
+          <Edit />
+        </Route>
+        <Router path="/">
+          <Houses
+          houses={houses}
+          handleDeleteHouse={handleDeleteHouse}
+          />
+        </Router>
+      </Switch>
+    </Router>
   );
 }
 
